5ce0b77312d675318ae14a140a25237dafde0a9a
sven-gerlach <svengerlach@me.com>
2021-10-13 15:07:01 -0400

    MapGL doesn't work with React properly. Need to switch back to Google Maps

--- src/views/reserve/Map/Map.js
+++ src/views/reserve/Map/Map.js
@@ -1,34 +1,13 @@
 import React, {Component} from "react";
-import ReactMapGL from "react-map-gl"
 
-// todo: React cannot read any env vars defined inside the dyno
-// const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
-
+const token = process.env.REACT_APP_SESSION_ENCRYPTION_KEY
+console.log(token)
 
 class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewport: {
-        latitude: 40.760551,
-        longitude: -73.997561,
-        zoom: 14,
-        bearing: 0,
-        pitch: 0
-      }
-    };
-  }
 
   render() {
     return (
-      <ReactMapGL
-        {...this.state.viewport}
-        width="100%"
-        height="100%"
-        mapStyle="mapbox://styles/mapbox/dark-v9"
-        onViewportChange={(viewport) => this.setState({ viewport })}
-        mapboxApiAccessToken="pk.eyJ1Ijoic3ZlbmdlcmxhY2giLCJhIjoiY2t1b3p1bjAxNGgwODMxbzNpdmRxZHpxayJ9.XKYHiZs-lwZzw-i1r0QrGg"
-      />
+      <p>[Map]</p>
     )
   }
 }
