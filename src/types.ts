7ae232f71d45e7fe75c1cdcbe3b8521268623892
sven-gerlach <svengerlach@me.com>
2021-10-18 19:46:51 -0400

    Add basic content of reserve summary view, store generic types in types.ts

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface ParkingSpot {
+  id: string,
+  lat: string,
+  lng: string,
+  rate: string,
+  reserved: boolean,
+  created_at: string,
+  updated_at: string,
+}
--- src/views/reserveSummary/reserveSummary.tsx
+++ src/views/reserveSummary/reserveSummary.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import PageTitle from "../../components/pageTitle/PageTitle";
+import { RouteComponentProps } from "react-router-dom";
 
+// import utils
+import { round } from "lodash";
+
+// Import interfaces
+import { ParkingSpot } from "../../types";
 
 // Interfaces
 interface Props {
-  availableParkingSpots: [],
-  history: {},
-  location: {},
-  match: {}
+  availableParkingSpots: ParkingSpot[],
+}
+interface RouteParams {
+  id: string,
 }
 
 /**
@@ -16,14 +22,26 @@ interface Props {
  * Booking confirmation: parkingSpotId, whatThreeWords, gpsCoordinates, rates (/hour, /min)
  * User input: reservationLength, alertSubscription
  */
-class ReserveSummary extends Component<Props> {
+class ReserveSummary extends Component<Props & RouteComponentProps<RouteParams>> {
   render() {
-    console.log(this.props)
+    const parkingSpotId = this.props.match.params.id
+    const parkingSpot = this.props.availableParkingSpots.filter(parkingSpot => parkingSpot.id == parkingSpotId)[0]
+    const parkingSpotGps = `Latitude: ${parkingSpot.lat} / Longitude: ${parkingSpot.lng}`
+
     return (
       <>
         <PageTitle titleText="Reservation Summary" />
         <h3>Parking Spot ID</h3>
-        {/*<p>{this.props.match.params}</p>*/}
+        <p>{parkingSpotId}</p>
+        <h3>GPS Coordinates</h3>
+        <p>{parkingSpotGps}</p>
+        <h3>What Three Words</h3>
+        {/* todo: action API call to WTW to convert GPS to whatthreewords */}
+        <p>[to come]</p>
+        <h3>Rate ($ / hour)</h3>
+        <p>{parkingSpot.rate}</p>
+        <h3>Rate ($ / min)</h3>
+        <p>{round(Number(parkingSpot.rate) / 60, 2)}</p>
       </>
     )
   }
