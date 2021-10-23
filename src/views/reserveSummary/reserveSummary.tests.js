e46531c05b7ce5df111d73a88b3da14e626c8068
sven-gerlach <svengerlach@me.com>
2021-10-23 16:58:25 -0400

    Add tests to reserveSummary
    On branch res-sum-tests
    Changes to be committed:
    modified:   src/views/reserveSummary/reserveSummary.tests.js
    
    test 1: if the parking spot currently reviewed is no longer available
    for reserving, a modal pops up
    
    test 2: if the rate of a parking spot currently reviewed changes, this
    change is immediately reflected on the reserveSummary view

--- src/views/reserveSummary/reserveSummary.tests.js
+++ src/views/reserveSummary/reserveSummary.tests.js
@@ -15,9 +15,13 @@ import ReserveSummary from "./reserveSummary";
 // mock getObjectFromStorage function that retrieves the considered parking spot from session storage
 jest.mock("../../utils/sessionStorage", () => ({ getObjectFromStorage: jest.fn() }))
 
+// sandbox variable which needs to be accessed within test scopes
+let sandboxParkingSpot1
+let sandboxParkingSpot2
+let sandboxRerender
 
 describe("Test the reserve summary component", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     // mock available parking spots array
     const parkingSpot1 = parkingSpotFixture()
     const parkingSpot2 = parkingSpotFixture()
@@ -26,11 +30,15 @@ describe("Test the reserve summary component", () => {
       parkingSpot2
     ]
 
+    // assign variables to global space so they can be read inside test scopes
+    sandboxParkingSpot1 = parkingSpot1
+    sandboxParkingSpot2 = parkingSpot2
+
     // mock getObjectFromStorage
     getObjectFromStorage.mockImplementation(() => parkingSpot1)
 
     // render the page
-    render(
+    const { rerender } = render(
       <BrowserRouter>
         <Route render={props => (
           <ReserveSummary
@@ -42,17 +50,81 @@ describe("Test the reserve summary component", () => {
         <Route path="/payment" render={() => <h1>Payment Page</h1>} />
       </BrowserRouter>
     )
+
+    //  assign rerender to sandbox variable
+    sandboxRerender = rerender
   })
 
   test("Back button returns user to the map page", () => {
+    // click on the back button
+    userEvent.click(screen.getByRole("button", { name: "Back" }))
+
+    // assertions
+    expect(screen.getByText("Reservation Page")).toBeInTheDocument()
+  })
+
+  test("Payment button redirects user to the /payment route", () => {
+    // click on the Payment button
     userEvent.click(screen.getByRole("button", { name: "Payment" }))
 
     // assertions
     expect(screen.getByText("Payment Page")).toBeInTheDocument()
   })
 
-  test.todo("Payment button redirects user to the /payment route")
+  test("If parking spot becomes unavailable a modal with text and one button is rendered to the DOM", () => {
+    // remove parkingSpot1 from availableParkingSpots
+    const availableParkingSpots = [
+      sandboxParkingSpot2
+    ]
 
-  test.todo("If current parking spot becomes unavailable a modal pops up with some text and one button that" +
-    " sends the user back to the map page")
+    // re-render the view
+    sandboxRerender(
+      <BrowserRouter>
+        <Route render={props => (
+          <ReserveSummary
+            availableParkingSpots={availableParkingSpots}
+            history={props.history}
+          />
+        )} />
+        <Route path="/reserve" render={() => <h1>Reservation Page</h1>} />
+        <Route path="/payment" render={() => <h1>Payment Page</h1>} />
+      </BrowserRouter>
+    )
+
+    // assert that the screen has a button with text "Find Alternative Parking"
+    expect(screen.getByRole("button", { name: "Find Alternative Parking" })).toBeInTheDocument()
+  })
+
+  test("If parking spot details change (e.g. rate) then this change renders to the DOM", () => {
+    // parking spot rates
+    const oldParkingSpot1Rate = sandboxParkingSpot1.rate
+    const newParkingSpot1Rate = oldParkingSpot1Rate === 20 ? 30 : 20
+
+    // create new parkingSpot1
+    sandboxParkingSpot1.rate = 20
+
+    // change parkingSpot1 details
+    const availableParkingSpots = [
+      sandboxParkingSpot1,
+      sandboxParkingSpot2
+    ]
+
+    // rerender the component
+    sandboxRerender(
+      <BrowserRouter>
+        <Route render={props => (
+          <ReserveSummary
+            availableParkingSpots={availableParkingSpots}
+            history={props.history}
+          />
+        )} />
+        <Route path="/reserve" render={() => <h1>Reservation Page</h1>} />
+        <Route path="/payment" render={() => <h1>Payment Page</h1>} />
+      </BrowserRouter>
+    )
+
+    // assertions
+    expect(oldParkingSpot1Rate).not.toBe(newParkingSpot1Rate)
+    expect(screen.getByText(newParkingSpot1Rate)).toBeInTheDocument()
+  })
 })
