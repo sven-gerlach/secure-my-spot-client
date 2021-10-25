4becca3d482d2637905d0d2ab9e9cc4434cd07df
sven-gerlach <svengerlach@me.com>
2021-10-24 21:06:24 -0400

    Add function that makes the http call to the api to create a new reservation resource

---
+++ src/views/reserve/reserveSummary/payment/Payment.tsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { RouteComponentProps } from "react-router-dom";
+
+// import utils
+import { createParkingSpotReservation } from "../../../../httpRequests/parkingSpots";
+
+interface IProps {
+  reservationLength: string,
+  parkingSpotId: string
+}
+
+interface IState {
+  email: string
+}
+
+
+class Payment extends Component<RouteComponentProps & IProps, IState> {
+  constructor(props: RouteComponentProps & IProps) {
+    super(props)
+    this.state = {
+      email: ""
+    }
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    this.setState({ email: value })
+  }
+
+  handlePayment = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    // construct data sent to API
+    const data = {
+      email: this.state.email,
+      reservationLength: this.props.reservationLength
+    }
+
+    // send http post request to api to create a new reservation resource
+    createParkingSpotReservation(this.props.parkingSpotId, data)
+      .then(res => {
+        // todo: implement logic once parking spot has been reserved
+        // need to received a reservation ID
+        // combination of email and reservation id should allow the user to amend the reservation under My Reservations
+        console.log(res)
+      })
+      .catch(e => console.log(e))
+  }
+
+  render() {
+    return (
+      <>
+        <input
+          value={this.state.email}
+          onChange={this.handleChange}
+          placeholder="e-Mail"
+        />
+        <button
+          onClick={this.handlePayment}
+        >Confirm Payment</button>
+      </>
+    )
+  }
+}
+
+export default Payment
